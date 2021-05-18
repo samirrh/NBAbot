7b71e85e509017ffb2409d26a6939a603e6079a6
samirrh <66929978+samirrh@users.noreply.github.com>
2021-05-18 12:26:52 -0400

    cleaned up

--- main.py
+++ main.py
@@ -5,9 +5,8 @@ from bs4 import BeautifulSoup
 from discord.ext import commands
 bot = commands.Bot(command_prefix='$')
 
-import random
+import random # for random color embeds
 
-#embed=discord.Embed(title="Player of the Week", url='https://basketball.realgm.com/nba/awards/by-type/Player-Of-The-Week/30', color=0x159619)
 def get_pow():
   url = 'https://basketball.realgm.com/nba/awards/by-type/Player-Of-The-Week/30'
   embed=discord.Embed(title="Player of the Week", url='https://basketball.realgm.com/nba/awards/by-type/Player-Of-The-Week/30', color=0x159619)
@@ -40,9 +39,6 @@ def get_pow():
       playersInfo[tds[2].text]['age']=tds[8].text
   embed.add_field(name="Eastern Conference Player of the Week", value=playersInfo['East']['name'], inline=False)
   embed.add_field(name="Eastern Conference Player of the Week - "+playersInfo['date'], value=playersInfo['East']['name'], inline=False)
-  #embed.set_thumbnail(url="https://i.imgur.com/axLm3p6.jpeg")
-  #embed.set_image(url="https://www.basketball-reference.com/req/202104203/images/players/tatumja01.jpg")
-  #embed.set_image(url="https://www.basketball-reference.com/req/202104203/images/players/bookede01.jpg")
   embed.add_field(name="Team", value=playersInfo['East']['team'], inline=True)
   embed.add_field(name="Position", value=playersInfo['East']['pos'], inline=True)
   embed.add_field(name="Age", value=playersInfo['East']['age'], inline=True)
@@ -52,14 +48,15 @@ def get_pow():
   embed.add_field(name="Age", value=playersInfo['West']['age'], inline=True)
   return embed
 
-#refactor function 
 def get_powWest(weeksAgo):
+  random_number = random.randint(0,167776)
+  randomColor = random_number
   if weeksAgo == 0:
     targetIndex = 1
   else:
     targetIndex = weeksAgo*2+1
   statURL = 'https://basketball.realgm.com/nba/awards/by-type/Player-Of-The-Week/30'
-  embed=discord.Embed(title="Western Conference Player of the Week", url=statURL, color=0x159619)
+  embed=discord.Embed(title="Western Conference Player of the Week", url=statURL, color=randomColor)
   response = requests.get(statURL)
   playerInfo = {
     'date':'',
@@ -96,13 +93,15 @@ def get_powWest(weeksAgo):
     embed.set_footer(text='Player of the Week Recipient on ' + playerInfo['date'] + ' - ' + 'Approx '+str(weeksAgo)+' weeks ago.')
   return embed
 
-def get_powEast():
+def get_powEast(weeksAgo):
   random_number = random.randint(0,167776)
   randomColor = random_number
-  url = 'https://basketball.realgm.com/nba/awards/by-type/Player-Of-The-Week/30'
-  embed=discord.Embed(title="Eastern Conference Player of the Week", url=url, color=randomColor)
-  response = requests.get(url)
+  targetIndex = weeksAgo*2
+  statURL = 'https://basketball.realgm.com/nba/awards/by-type/Player-Of-The-Week/30'
+  embed=discord.Embed(title="Eastern Conference Player of the Week", url=statURL, color=randomColor)
+  response = requests.get(statURL)
   playerInfo = {
+    'date':'',
     'name':'',
     'team':'',
     'pos':'',
@@ -111,19 +110,20 @@ def get_powEast():
   soup = BeautifulSoup(response.content, 'html.parser')
   table = soup.find('table', attrs={'class':'tablesaw','data-tablesaw-mode':'swipe'}).tbody
   trs = table.find_all('tr')
-  for count, tr in enumerate(trs):
-    if count == 0:
-      print(count)
+  for index, tr in enumerate(trs):
+    if index == targetIndex:
+      print(index)
       tds = tr.find_all('td')
+      playerInfo['date']=tds[3].text
       playerInfo['name']=tds[1].text
       playerInfo['team']=tds[4].text
       playerInfo['pos']=tds[5].text
       playerInfo['age']=tds[8].text
-    elif count > 0:
-      print(count)
+    elif index > targetIndex:
+      print(index)
       break
     else:
-      print(count)
+      print(index)
       continue
   urlId = playerInfo['name'].split()[1].lower()[:5] + playerInfo['name'].split()[0].lower()[:2] + '01.jpg'
   embed.add_field(name="Name", value=playerInfo['name'], inline=False)
@@ -131,6 +131,8 @@ def get_powEast():
   embed.add_field(name="Position", value=playerInfo['pos'], inline=True)
   embed.add_field(name="Age", value=playerInfo['age'], inline=True)
   embed.set_image(url="https://www.basketball-reference.com/req/202104203/images/players/" + urlId)
+  if weeksAgo > 0:
+    embed.set_footer(text='Player of the Week Recipient on ' + playerInfo['date'] + ' - ' + 'Approx '+str(weeksAgo)+' weeks ago.')
   return embed
 
 
@@ -140,7 +142,7 @@ async def on_ready():
 
 @bot.command()
 async def ECpow(ctx, arg=0):
-    await ctx.send(embed=get_powEast())
+    await ctx.send(embed=get_powEast(arg))
 
 @bot.command()    
 async def WCpow(ctx, arg=0):
